43a0eeb5ecf10e8b29fbd19754af1e247f4ebbb2
saptan <ahmed.abdulmomen@3ddx.com>
2020-03-15 21:42:10 +0200

    gg

--- index.js
+++ index.js
@@ -1,32 +1,54 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
 
-const usersRouter = require('./routes/api/users');
-const authRouter = require('./routes/api/auth');
-const profileRouter = require('./routes/api/profile');
-const postsRouter = require('./routes/api/posts');
+const usersRouter = require("./routes/api/users");
+const authRouter = require("./routes/api/auth");
+const profileRouter = require("./routes/api/profile");
+const postsRouter = require("./routes/api/posts");
 
-require('./db/db')();
+require("./db/db")();
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(helmet());
 
-app.use('/api/users', usersRouter);
-app.use('/api/posts', postsRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/profile', profileRouter);
-
-app.get('/', (req, res) => {
-	res.send('API');
-});
+app.use("/api/users", usersRouter);
+app.use("/api/posts", postsRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/profile", profileRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-	console.log(`Server is start at port : ${PORT}`);
+  console.log(`Server is start at port : ${PORT}`);
 });
 
+// const genSwaggerJson = () => {
+//   const routes = [];
+
+//    .forEach(middleware => {
+//     if (middleware.route) {
+//       routes.push(
+//         `${Object.keys(middleware.route.methods)} -> ${middleware.route.path}`
+//       );
+//     }
+//   });
+
+//   console.log(JSON.stringify(routes, null, 2));
+// };
+
+// genSwaggerJson();
+
+console.log("*******************************************");
+app._router.stack.forEach(layer => {
+  if (layer.handle.stack) {
+    console.log(layer.handle.stack[0].route);
+    console.log(layer.handle.stack.length);
+    console.log("---------------------");
+  }
+});
+
+console.log("**********************************************");
