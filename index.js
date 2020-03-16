a3f14d37c45b77a61daf8053a7ebb422cc7cf8a8
ahmedsaptan <ahmed.sayed.fcis1997@gmail.com>
2020-03-16 08:45:07 +0200

    tets

--- index.js
+++ index.js
@@ -1,29 +1,74 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const helmet = require("helmet");
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const swaggerUi = require('swagger-ui-express');
 
-const usersRouter = require("./routes/api/users");
-const authRouter = require("./routes/api/auth");
-const profileRouter = require("./routes/api/profile");
-const postsRouter = require("./routes/api/posts");
+const usersRouter = require('./routes/api/users');
+const authRouter = require('./routes/api/auth');
+const profileRouter = require('./routes/api/profile');
+const postsRouter = require('./routes/api/posts');
 
-require("./db/db")();
+require('./db/db')();
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 app.use(helmet());
 
-app.use("/api/users", usersRouter);
-app.use("/api/posts", postsRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/profile", profileRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/posts', postsRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/profile', profileRouter);
+
+const objs = {
+  "swagger": "2.0",
+  "info": {
+      "version": "1.0.0",
+      "title": "Simple API",
+      "description": "A simple API to learn how to write OpenAPI Specification"
+  },
+  "schemes": [
+      "https"
+  ],
+  "host": "simple.api",
+  "basePath": "/openapi101",
+  "paths": {
+      "/persons": {
+          "get": {
+              "summary": "Gets some persons",
+              "description": "Returns a list containing all persons.",
+              "responses": {
+                  "200": {
+                      "description": "A list of Person",
+                      "schema": {
+                          "type": "array",
+                          "items": {
+                              "properties": {
+                                  "firstName": {
+                                      "type": "string"
+                                  },
+                                  "lastName": {
+                                      "type": "string"
+                                  },
+                                  "username": {
+                                      "type": "string"
+                                  }
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+      }
+  }
+}
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(objs));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is start at port : ${PORT}`);
+	console.log(`Server is start at port : ${PORT}`);
 });
 
 // const genSwaggerJson = () => {
@@ -42,13 +87,13 @@ app.listen(PORT, () => {
 
 // genSwaggerJson();
 
-console.log("*******************************************");
+console.log('*******************************************');
 app._router.stack.forEach(layer => {
-  if (layer.handle.stack) {
-    console.log(layer.handle.stack[0].route);
-    console.log(layer.handle.stack.length);
-    console.log("---------------------");
-  }
+	if (layer.handle.stack) {
+		console.log(layer.handle.stack[0].route);
+		console.log(layer.handle.stack.length);
+		console.log('---------------------');
+	}
 });
 
-console.log("**********************************************");
+console.log('**********************************************');
